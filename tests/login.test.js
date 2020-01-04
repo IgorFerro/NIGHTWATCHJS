4a4f8423b113dab4ec280d870d88cfec26bb624f
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-01-04 18:58:46 -0300

    mutiples steps

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'login com sucesso': function(browser){
+    /*'login com sucesso': function(browser){
         var userInfo = '.user .info span'
         browser
         .maximizeWindow()
@@ -12,5 +12,25 @@ module.exports = {
         .assert.containsText(userInfo, 'Quilombo')
         .end();
 
+    },*/
+
+    'Given i access the log page':function(browser){
+        browser
+        .maximizeWindow()
+        .url('http://zombie-web:5000/login')
+        .waitForElementVisible('.card-login', 3000)
+    },
+    'When i logged':function(browser){
+       browser
+        .setValue('input[name=email]','zumbi@dospalmares.com.br')
+        .setValue('input[name=password]','pwd123')
+        .click('.login-button')
+    },
+    'Then i will see my name in login area':function(browser){
+        var userInfo = '.user .info span'
+        browser
+        .waitForElementVisible(userInfo, 3000)
+        .assert.containsText(userInfo, 'Quilombo')
+        .end();
     }
 }
